96b7a310183003d778cd21124d534dfb62b68679
Remix Workspace <>
2022-09-13 16:03:15 +0530

    new  Folder added

---
+++ variablesIn Soliditiy/variables.sol
@@ -0,0 +1,29 @@
+// variables are reserved memory locations  to store values
+// used to store information and to be referanced and manipulated to computer program
+// Types: Boolean - keyword:bool -value: True/False
+// Integer: keyword : uint - value-signed and unsigned integer of varying sizes
+// String : keyword : string : data values tht are made up of ordered sequences of characters
+
+// store-owner = 300
+// chocolate-bar = 20
+// total-value = store-owner +choclate-bar;
+
+// Main type sof vairables
+// Booleans
+// lieDetector = boolean True or False
+// Integers
+// walletAmount = integer would hold the amount
+// Strings
+// errorMessageText = string that would say 'Error'  
+
+// Solidity Variables
+pragma solidity >=0.8.0 < 0.9.0;
+
+contract LearnSolidityVariables {
+    // all the codes come here
+    uint chocolateBar = 100;
+    uint storeOwner   = 300;
+    bool lieDetector  = true;
+    string errorMessageText = 'There is an mistake';
+    
+}
